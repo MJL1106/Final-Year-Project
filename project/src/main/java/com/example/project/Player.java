6bb043c7812179ac8794a9003e0ed7161ccec808
MJL1106 <75175998+MJL1106@users.noreply.github.com>
2022-10-11 20:28:05 +0100

    Implemented more setters and getters
    Added more setters and getters for the player object along with TDD and javadoc

--- project/src/main/java/com/example/project/Player.java
+++ project/src/main/java/com/example/project/Player.java
@@ -1,13 +1,68 @@
 package com.example.project;
 
+/**
+ * Class to create a Player.
+ */
 public class Player {
-    private String playerName;
+    private String playerName,choice;
+    private int jailTime;
 
-    public Player(String playerName){
-        this.playerName = playerName;
+    /**
+     * A constructor to create a new Player.
+     * 
+     * @param name
+     */
+    public Player(String name){
+        this.playerName = name;
     }
 
+    
+    /** 
+     * Gets the name of the Player.
+     * 
+     * @return String name set by Player
+     */
     public String getPlayerName(){
         return this.playerName;
+    }
+
+    
+    /** 
+     * Sets the Players choice of action.
+     * 
+     * @param str is the entered decision
+     */
+    public void setChoice(String str){
+        this.choice = str;
+    }
+
+    
+    /** 
+     * Gets the Players choice of action.
+     * 
+     * @return String choice of Player
+     */
+    public String getChoice(){
+        return this.choice;
+    }
+
+    
+    /** 
+     * Sets the jail time of the Player.
+     * 
+     * @param time result of dilemma
+     */
+    public void setJailTime(int time){
+        this.jailTime = time;
+    }
+
+    
+    /** 
+     * Gets the Players jail time.
+     * 
+     * @return int total time in jail
+     */
+    public int getJailTime(){
+        return this.jailTime;
     }
 }
--- project/src/test/java/com/example/project/TestPlayer.java
+++ project/src/test/java/com/example/project/TestPlayer.java
@@ -10,4 +10,18 @@ public class TestPlayer {
         Player p1 = new Player("Matthew");
         assertEquals(p1.getPlayerName(), "Matthew", "Testing if the player name has been initialised");
     }
+
+    @Test
+    public void testChoice() {
+        Player p1 = new Player("Matthew");
+        p1.setChoice("defect");
+        assertEquals(p1.getChoice(), "defect", "Testing if the player choice has been set");
+    }
+
+    @Test
+    public void testJailTime() {
+        Player p1 = new Player("Matthew");
+        p1.setJailTime(10);
+        assertEquals(p1.getJailTime(), 10, "Testing if the player choice has been set");
+    }
 }
