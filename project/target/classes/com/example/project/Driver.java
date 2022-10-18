3a0cdab0089765108386d5917f2f301954d5d2b3
MJL1106 <75175998+MJL1106@users.noreply.github.com>
2022-10-18 14:34:46 +0100

    Created terminal interface
    Written basic code to allow players to enter their name and choice in the terminal and an outputted jail time is given.

--- project/target/classes/com/example/project/Driver.java
+++ project/target/classes/com/example/project/Driver.java
@@ -1,13 +1,36 @@
 package com.example.project;
 
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * Main class that runs the interface for the player.
  *
  */
 public class Driver 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Tests" );
+        Scanner userInput = new Scanner(System.in);
+        String playerName;
+        String playerChoice;
+        System.out.println( "Prisoner's Dilemma" );
+        
+        System.out.println("Player 1 enter your name: ");
+        playerName = userInput.nextLine();
+        Player p1 = new Player(playerName);
+
+        System.out.println("Player 2 enter your name: ");
+        playerName = userInput.nextLine();
+        Player p2 = new Player(playerName);
+
+        System.out.println(p1.getPlayerName() + " do you want to cooperate or defect: ");
+        playerChoice = userInput.nextLine();
+        p1.setChoice(playerChoice);
+
+        System.out.println(p2.getPlayerName() + " do you want to cooperate or defect: ");
+        playerChoice = userInput.nextLine();
+        p2.setChoice(playerChoice);
+
+        System.out.println(Dilemma.compareChoice(p1,p2));
     }
 }
