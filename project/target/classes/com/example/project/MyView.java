f725b130f81a2b06f39fbfec54428e3426b70cec
MJL1106 <75175998+MJL1106@users.noreply.github.com>
2022-11-09 11:34:56 +0000

    Edited GUI for iteration
    Changed the gui to allow players to play as many round as they want against each other and total points is displayed after every round.

--- project/target/classes/com/example/project/MyView.java
+++ project/target/classes/com/example/project/MyView.java
@@ -8,6 +8,8 @@ import javafx.scene.control.TextField;
 
 public class MyView {
 
+    private Player p1,p2;
+
     @FXML
     private TextField Choice1;
 
@@ -24,14 +26,25 @@ public class MyView {
     private Button compare;
 
     @FXML
+    private Button names;
+
+    @FXML
     private TextArea taOutput;
 
     @FXML
+    void enterNames(ActionEvent event) {
+        p1 = new Player(Name1.getText());
+        p2 = new Player(Name2.getText());
+    }
+
+    @FXML
     void showJailTime(ActionEvent event) {
-        Player p1 = new Player(Name1.getText());
-        Player p2 = new Player(Name2.getText());
         p1.setChoice(Choice1.getText());
         p2.setChoice(Choice2.getText());
+        Iterator.tallyPoints(p1,p2);
+        taOutput.setText(p1.getPlayerName() + " points: " + p1.getJailTime() + "\n" + p2.getPlayerName() + " points: " + p2.getJailTime());
+        Choice1.setText("");
+        Choice2.setText("");
 
     }
 
